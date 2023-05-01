6b5d72a7a4bc4a71b2321b11f8a8da3ec7cbeef8
Asif-Abdullah21 <asifabdullah214365@gmail.com>
2023-05-02 00:05:05 +0600

    Shape 2

--- T_Shape_2.c
+++ T_Shape_2.c
@@ -6,6 +6,37 @@ int main()
 
     int s=n-1,k=1;
 
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= s; j++)
+        {
+            printf(" ");
+        }
+        for (int j = 1; j <= k; j++)
+        {
+            printf("*");
+        }
+
+        k+=2;
+        s--;
+
+        printf("\n");
+        
+    }
+    
+    
+    return 0;
+}
+
+/*
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+
+    int s=n-1,k=1;
+
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < s; j++)
@@ -27,3 +58,5 @@ int main()
     
     return 0;
 }
+
+*/
